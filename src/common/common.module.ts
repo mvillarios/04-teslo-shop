518c6bf14d756766de9da27515063d57ab66704e
Miguel Villa Ríos <mvilla2019@inf.udec.cl>
2026-03-23 17:37:53 -0300

    feat: add pagination support and product tags, refactor product service methods

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ErrorHandleService } from './services/error-handle.service';
 
 @Module({})
 export class CommonModule {}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,5 +1,7 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { QueryFailedError, Repository } from 'typeorm';
+import { v4 as uuid, validate as isUuid } from 'uuid';
+import { PaginationDto } from '../common/dtos/pagination.dto';
 import { IErrorsTypeORM } from '../common/interfaces/errors-type-orm.interface';
 import { ErrorHandleService } from '../common/services/error-handle.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -10,6 +12,7 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 
 @Injectable()
@@ -30,22 +33,59 @@ export class ProductsService {
     }
   }
 
-  async findAll() {
-    return this.productsRepository.find();
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.productsRepository.find({
+      take: limit,
+      skip: offset,
+    });
   }
 
-  async findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(term: string) {
+    let product: Product | null;
+
+    if (isUuid(term)) {
+      product = await this.productsRepository.findOneBy({ id: term });
+    } else {
+      const queryBuilder = this.productsRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('UPPER(title) =:title or slug =:slug', {
+          title: term.toUpperCase(),
+          slug: term.toLowerCase(),
+        })
+        .getOne();
+    }
+
+    if (!product) {
+      throw new NotFoundException(
+        `Product with id, slug or title "${term}" not found`,
+      );
+    }
+    return product;
   }
 
-  async update(id: number, updateProductDto: UpdateProductDto) {
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productsRepository.preload({
+      id,
+      ...updateProductDto,
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id "${id}" not found`);
+    }
+
     try {
-    } catch (error: any) {
+      await this.productsRepository.save(product);
+      return product;
+    } catch (error) {
       this.errorHandler.errorHandle(error as IErrorsTypeORM);
     }
   }
 
-  async remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productsRepository.remove(product);
+    return;
   }
 }
